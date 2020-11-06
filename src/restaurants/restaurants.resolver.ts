da624956c95f6445824abae2813bf495af72b29d
Jason Park <zeipar@gmail.com>
2020-11-05 21:22:13 -0800

    Updated resolver-query-myrestaurant to return an array of Restaurants.

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -4,8 +4,8 @@ import { Restaurant } from './entities/restaurant.entity';
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
   //   @Query(() => Boolean), () can be replaced with 'returns', for visual.
-  @Query(returns => Restaurant)
-  myRestaurant() {
-    return true;
+  @Query(returns => [Restaurant])
+  myRestaurant(): Restaurant[] {
+    return [];
   }
 }
