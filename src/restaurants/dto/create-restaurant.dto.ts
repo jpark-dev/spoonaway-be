7e425face78c6d8a8bcc8a9789cec215a4447da6
Jason Park <zeipar@gmail.com>
2020-11-05 21:52:38 -0800

    Set class validator: @IsString(), @IsBoolean() and @Length().

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,18 +1,24 @@
 import { ArgsType, Field } from '@nestjs/graphql';
+import { IsBoolean, IsString, Length } from 'class-validator';
 
 // inputType() one object to pass to graphQL as an argument.
 // argsType() several values can be passed to graphQL without being an object
 @ArgsType()
 export class CreateRestaurantDto {
   @Field(typeIs => String)
+  @IsString()
+  @Length(5, 15)
   name: string;
 
   @Field(typeIs => Boolean)
+  @IsBoolean()
   isGood?: Boolean;
 
   @Field(typeIs => String)
+  @IsString()
   address: string;
 
   @Field(typeIs => String)
+  @IsString()
   ownerName: string;
 }
