07c1ba1b6c0bfab63741bab1100969203584498b
Jason Park <zeipar@gmail.com>
2020-11-05 21:23:45 -0800

    Added argument usinig @Args - veganOnly as boolean.

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,11 +1,11 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Resolver(of => Restaurant)
 export class RestaurantResolver {
   //   @Query(() => Boolean), () can be replaced with 'returns', for visual.
   @Query(returns => [Restaurant])
-  myRestaurant(): Restaurant[] {
+  myRestaurant(@Args('veganOnly') veganOnly: Boolean): Restaurant[] {
     return [];
   }
 }
