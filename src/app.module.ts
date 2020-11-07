42c7d7d7809e5d71f943b511c883dfdfed4b8467
Jason Park <zeipar@gmail.com>
2020-11-06 22:38:04 -0800

    Refactored DB connection info and used cross-env to choose which .env file to be used.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RestaurantsModule } from './restaurants/restaurants.module';
@@ -10,13 +11,18 @@ import { RestaurantsModule } from './restaurants/restaurants.module';
       autoSchemaFile: true,
     }),
     RestaurantsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.ENV === 'dev' ? '.env.dev' : '.env.test',
+      ignoreEnvFile: process.env.ENV === 'prod',
+    }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: '',
-      port: 5432,
-      username: 'postgres',
-      password: '',
-      database: 'spoonaway',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       synchronize: true,
       logging: true,
     }),
