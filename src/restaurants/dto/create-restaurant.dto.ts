c1b2226245280a23bbd113b60e1eaeaadddb296c
Jason Park <zeipar@gmail.com>
2020-11-08 00:53:28 -0800

    In restaurantDto, Used OmitType() to inherit from the entity. Validation would happen in the entity now.

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,24 +1,12 @@
-import { ArgsType, Field } from '@nestjs/graphql';
-import { IsBoolean, IsString, Length } from 'class-validator';
+import { InputType, OmitType } from '@nestjs/graphql';
+import { Restaurant } from '../entities/restaurant.entity';
 
 // inputType() one object to pass to graphQL as an argument.
 // argsType() several values can be passed to graphQL without being an object
-@ArgsType()
-export class CreateRestaurantDto {
-  @Field(typeIs => String)
-  @IsString()
-  @Length(5, 15)
-  name: string;
-
-  @Field(typeIs => Boolean)
-  @IsBoolean()
-  isGood?: Boolean;
-
-  @Field(typeIs => String)
-  @IsString()
-  address: string;
-
-  @Field(typeIs => String)
-  @IsString()
-  ownerName: string;
-}
+@InputType()
+// instead of extending partialTypes(), we can add InputType() in the entity and make it an abstract type => @InputType({ isAbstract: true})
+export class CreateRestaurantDto extends OmitType(
+  Restaurant,
+  ['id'],
+  InputType,
+) {}
