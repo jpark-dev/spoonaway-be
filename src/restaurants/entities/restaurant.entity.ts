9faff7c0a5291780dc79efea18e57c99bcae6037
Jason Park <zeipar@gmail.com>
2020-11-07 18:10:33 -0800

    Added typeorm decorators for restaurant entity columns with a primary id.

--- src/restaurants/entities/restaurant.entity.ts
+++ src/restaurants/entities/restaurant.entity.ts
@@ -1,17 +1,27 @@
 import { Field, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @ObjectType()
+@Entity()
 export class Restaurant {
   // '() =>' can be replaced with 'typeIs =>'
+  @Field(typeIs => Number)
+  @PrimaryGeneratedColumn()
+  id: number;
+
   @Field(typeIs => String)
+  @Column()
   name: string;
 
   @Field(typeIs => Boolean, { nullable: true })
+  @Column()
   isGood?: Boolean;
 
   @Field(typeIs => String)
+  @Column()
   address: string;
 
   @Field(typeIs => String)
+  @Column()
   ownerName: string;
 }
