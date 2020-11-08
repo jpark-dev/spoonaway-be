6465cc8b17df6c986a4a259f8a0ec4269a96683e
Jason Park <zeipar@gmail.com>
2020-11-08 00:30:13 -0800

    Added restaurant creation method using this.respository.create(dto). Used this.repository.save in order to save to DB.

--- src/restaurants/restaurants.service.ts
+++ src/restaurants/restaurants.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateRestaurantDto } from './dto/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
 
 @Injectable()
@@ -11,5 +12,14 @@ export class RestaurantService {
   ) {}
   getAll(): Promise<Restaurant[]> {
     return this.restaurants.find();
+  }
+  createRestaurant(
+    createRestaurantDto: CreateRestaurantDto,
+  ): Promise<Restaurant> {
+    // instead of below code, typeorm provides an easier way to instantiate & create
+    // const newRestaurant = new Restaurant();
+    // newRestaurant.name = createRestaurantDto.name;
+    const newRestaurant = this.restaurants.create(createRestaurantDto);
+    return this.restaurants.save(newRestaurant);
   }
 }
