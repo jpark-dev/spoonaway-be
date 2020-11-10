e01dad009dba0593506a31d34c3762549e54d4e1
Jason Park <zeipar@gmail.com>
2020-11-09 23:32:08 -0800

    Added createdAt/updatedAt columns and inherited them from user.

--- src/common/entities/core.entity.ts
+++ src/common/entities/core.entity.ts
@@ -1,6 +1,16 @@
-import { PrimaryGeneratedColumn } from 'typeorm';
+import {
+  CreateDateColumn,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 
 export class CoreEntity {
   @PrimaryGeneratedColumn()
   id: number;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
