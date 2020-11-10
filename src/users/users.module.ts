bbc780ab6b0432249dd7ffc2c88594fb6c73a1b3
Jason Park <zeipar@gmail.com>
2020-11-09 23:37:58 -0800

    Created resolver/service for users. Added repository of User to be used in them.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
 
-@Module({})
+@Module({
+  // setting up a repository
+  imports: [TypeOrmModule.forFeature([User])],
+})
 export class UsersModule {}
