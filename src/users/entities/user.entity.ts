ac4e6569f47d2ef972fff7220c89f7c975c168ed
Jason Park <zeipar@gmail.com>
2020-11-09 23:29:06 -0800

    Created a user entity.

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity } from "typeorm";
+
+type UserRole = "client" | "owner" | "delivery";
+
+@Entity()
+export class User {
+
+    @Column()
+    email: string,
+
+    @Column()
+    password: string,
+
+    @Column()
+    role: UserRole,
+
+}
