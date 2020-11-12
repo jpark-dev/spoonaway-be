46cfa7433e1638d276bb52f1281916298683f9fc
Jason Park <zeipar@gmail.com>
2020-11-11 23:47:35 -0800

    Created a pseudocode and typescript function for the login.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateAccountInput } from './dto/create-account.dto';
+import { LoginInput } from './dto/login.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -29,5 +30,22 @@ export class UsersService {
       return { ok: false, error: 'Failed to create an account.' };
     }
     // if true, create user & hash pwd
+  }
+
+  async login({
+    email,
+    password,
+  }: LoginInput): Promise<{ ok: boolean; error?: string; token?: string }> {
+    // find user with email
+    // check if pwd matches
+    // make a JWT and pass it to the client
+
+    try {
+    } catch (error) {
+      return {
+        ok: false,
+        error,
+      };
+    }
   }
 }
