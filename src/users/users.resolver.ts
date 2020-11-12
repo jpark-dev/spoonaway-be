dc394d0cf795b36b6c0932f86d023b12d31d52a3
Jason Park <zeipar@gmail.com>
2020-11-11 23:41:50 -0800

    created LoginInput and LoginOutput to inherit required elements.

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -3,6 +3,7 @@ import {
   CreateAccountInput,
   CreateAccountOutput,
 } from './dto/create-account.dto';
+import { LoginInput, LoginOutput } from './dto/login.dto';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 
@@ -31,4 +32,7 @@ export class UsersResolver {
       return { ok: false, error };
     }
   }
+
+  @Mutation(returns => LoginOutput)
+  async login(@Args('input') loginInput: LoginInput) {}
 }
