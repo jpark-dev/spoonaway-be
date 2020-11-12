f9cff015b11a76f34487883dfad0cf1873fae971
Jason Park <zeipar@gmail.com>
2020-11-11 22:43:10 -0800

    Added the base logic to do check if user exists before creating a user.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateAccountInput } from './dto/create-account.dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -8,4 +9,21 @@ export class UsersService {
   constructor(
     @InjectRepository(User) private readonly users: Repository<User>,
   ) {}
+
+  async createAccount({ email, password, role }: CreateAccountInput) {
+    // check new user -> mail.exists?
+    try {
+      const exists = await this.users.findOne({ email });
+      if (exists) {
+        // throw error
+        return;
+      }
+      await this.users.save(this.users.create({ email, password, role }));
+      return true;
+    } catch (e) {
+      console.log(e);
+      return;
+    }
+    // if true, create user & hash pwd
+  }
 }
