18d6c89c7b72272c7aba35f1a804d878ebbafb6a
Jason Park <zeipar@gmail.com>
2020-11-16 23:58:32 -0800

    Refactored so jwt module/service use jwt constants and interfaces.

--- src/jwt/jwt.service.ts
+++ src/jwt/jwt.service.ts
@@ -1,11 +1,12 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { JwtModuleOptions } from './interfaces/jwt-module-options.interface';
+import { JwtModuleOptions } from './jwt.interfaces';
+import { CONFIG_OPTIONS } from './jwt.constants';
 
 @Injectable()
 export class JwtService {
-  constructor(@Inject('BANANAS') private readonly options: JwtModuleOptions) {
-    console.log(options);
-  }
+  constructor(
+    @Inject(CONFIG_OPTIONS) private readonly options: JwtModuleOptions,
+  ) {}
   hello() {
     console.log('hello');
   }
