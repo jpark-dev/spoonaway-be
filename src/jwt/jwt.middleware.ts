16f542986b600f9eb7c43d23e834046ca87ceb0d
Jason Park <zeipar@gmail.com>
2020-11-20 22:12:34 -0800

    Used class component for JWTmiddleware as it is more suitable for giving specific entry points.

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,14 +1,16 @@
-// import { NestMiddleware } from '@nestjs/common';
+import { NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
 
-// export class JwtMiddleware implements NestMiddleware {
-//   use(req: Request, res: Response, next: NextFunction) {
-//     console.log(req.headers);
-//     next();
-//   }
-// }
-
-export function JwtMiddleware(req: Request, res: Response, next: NextFunction) {
-  console.log(req.headers);
-  next();
+export class JwtMiddleware implements NestMiddleware {
+  use(req: Request, res: Response, next: NextFunction) {
+    if("x-jwt" in req.headers) {
+      console.log(req.headers["x-jwt"])
+    }
+    next();
+  }
 }
+
+// export function JwtMiddleware(req: Request, res: Response, next: NextFunction) {
+//   console.log(req.headers);
+//   next();
+// }
