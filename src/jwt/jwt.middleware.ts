27ffc29da258de12e718e53968ec983db0194051
Jason Park <zeipar@gmail.com>
2020-11-24 23:17:10 -0800

    If jwt exists in the header, get it and decode it to see if it works.

--- src/jwt/jwt.middleware.ts
+++ src/jwt/jwt.middleware.ts
@@ -1,10 +1,17 @@
-import { NestMiddleware } from '@nestjs/common';
+import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
+import { JwtService } from './jwt.service';
 
+@Injectable()
 export class JwtMiddleware implements NestMiddleware {
+  constructor(private readonly jwtService: JwtService){}
   use(req: Request, res: Response, next: NextFunction) {
     if("x-jwt" in req.headers) {
-      console.log(req.headers["x-jwt"])
+      const token = req.headers["x-jwt"];
+      const decoded = this.jwtService.verify(token.toString());
+      if (typeof decoded === "object" && decoded.hasOwnProperty('id')) {
+        console.log(decoded['id']);
+      }
     }
     next();
   }
